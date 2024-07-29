55043c95ee39f5794048c1f6f89bbfb507e77bf2
Nathan McFarland <ngmcfarland@gmail.com>
2024-07-29 16:00:02 -0400

    Changed baseApi to use localhost and chmod-ed the build script

--- ui/src/Context.jsx
+++ ui/src/Context.jsx
@@ -1,6 +1,7 @@
 import { createContext } from 'react';
 
-const apiBase = `http://${window.location.hostname}:8000/api`;
+// const apiBase = `http://${window.location.hostname}:8000/api`;
+const apiBase = `http://localhost:8000/api`;
 
 const ColorModeContext = createContext();
 
